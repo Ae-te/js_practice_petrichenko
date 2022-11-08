bfbe8086148c8489c3d72d0df2a2c719c0f12d01
Анна Егорова <agen.egorova@gmail.com>
2022-11-08 15:32:09 +0300

    Сode from lesson 16, 17

--- js_basics/lecture_practice/16/js/script.js
+++ js_basics/lecture_practice/16/js/script.js
@@ -1,2 +1,48 @@
 "use strict";
 
+let num = 20;//глобальная переменная
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 10;//локальная переменная
+}
+
+showFirstMessage("Hello, World!");//Вызов функции
+console.log(num);//здесь выведется значение глобальной переменной, 
+//т.к. локальная виднеется только внутри блока
+
+// function calc(a, b)  {
+//     return (a + b);
+//     //console.log('jgfk');   unreachable code
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+//1)
+function ret() {//Function Declaration – классическая запись функции
+    let num = 50;
+
+    //длинный код
+
+
+    return num;//возвращается результат данной функции
+}
+
+const anotherNum = ret();//записали результат предыдущей функции 
+//в другую, внешнюю переменную
+console.log(anotherNum);
+
+//2)
+const logger = function() {//Function Expression
+    console.log("Hello");
+};//В конце такой функции {} ставится ;
+
+logger();//Вызов функции
+
+//3)
+const calc = (a, b) => {//стрелочная функция
+    console.log('1');
+    return a + b;
+};
