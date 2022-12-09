dcfa51840a7471808e2bc1f43a47865d1e04c1c2
Анна Егорова <agen.egorova@gmail.com>
2022-12-09 23:07:27 +0300

    Sent homework_4 (script_24)

--- js_basics/homework/24/js/script.js
+++ js_basics/homework/24/js/script.js
@@ -1,2 +1,108 @@
-"use strict";
+/* Задание на урок:
 
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+'use strict';
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+    //1) создали методы на основе бывших функций
+    start: function() {//проверяет, чтобы вводили именно число просмотренных фильмов
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+
+    remeberMyFilms: function() {
+        for(let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;// Чтобы откатить счетчик назад и запустить вопросы еще раз
+            }
+        }
+    },
+
+    detectPersonalLevel: function() {//Определить уровень кинонасмотренности
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    //2)
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    //3) 
+    writeYourGenres: function() {
+        for (let i = 1; i < 2; i++) {//для 1 способа: i <= 3;
+            // 1 способ
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            // if (genre == '' || genre == null) {
+            //     console.log('Вы ввели некорректные данные или не ввели их вовсе');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;//записываются жанры в массив
+            // //массив должен начинаться с нуля, поэтому i - 1, 
+            // //если этого не сделать, то 0-ой элемент будет empty(пустой элемент) 
+            // //и всего будет 4 элемента
+            // }
+
+            // 2 способ - альтернативный, через одну строчку
+            let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+            //переводит строку в нижний регистр, чтобы sort() сортировал элементы без учета регистра
+
+            if (genres == '' || genres == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ');//записываются жанры в массив
+                personalMovieDB.genres.sort();//отсортирует все по алфавиту
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    }
+};
